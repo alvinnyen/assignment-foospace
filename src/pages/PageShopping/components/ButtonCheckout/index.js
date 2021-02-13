111a9dd34b096d5fc0f023cc49362f00284a77e3
alvinnyen <alvinyen219@gmail.com>
2021-02-14 02:27:11 +0800

    apply ContextPage and ContextProcessedProducts to witch to PAGE_SHOPPING_CHECKOUT_RESULT by clicking ButtOnCheckout

--- src/pages/PageShopping/components/ButtonCheckout/index.js
+++ src/pages/PageShopping/components/ButtonCheckout/index.js
@@ -1,16 +1,25 @@
 import { useContext } from "react";
 
+import { pages } from "../../../../App/App";
+
 import contextProductList from "../../../../contexts/ContextProductList";
+import contextPage from "../../../../contexts/ContextPage";
+import contextProcessedProducts from "../../../../contexts/ContextProcessedProducts";
+
 import utilGetProcessedProducts from "./utils";
 
 export default (props = {}) => {
   const { productList = "", setProductList = () => {} } = useContext(
     contextProductList
   );
+  const { setPage = () => {} } = useContext(contextPage);
+  const { setProcessedProducts = () => {} } = useContext(
+    contextProcessedProducts
+  );
 
   const buttonDisabled = !productList;
 
-  const handleCheckoutButtOnClick = () => {
+  const handleButtOnCheckoutClick = () => {
     const productListArray = productList.split(",");
     const { productsData = {} } = props;
     // console.log(productList);
@@ -21,14 +30,16 @@ export default (props = {}) => {
       productListArray,
       productsData
     );
-    console.log(processedProducts);
-    console.log(" ");
+
+    // TODO: check the length of processedProducts ?
+    setProcessedProducts(processedProducts);
+    setPage(pages.PAGE_SHOPPING_CHECKOUT_RESULT);
   };
 
   return (
     <button
       className="checkout-button"
-      onClick={handleCheckoutButtOnClick}
+      onClick={handleButtOnCheckoutClick}
       disabled={buttonDisabled}
     >
       Checkout
