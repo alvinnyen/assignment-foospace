c9e0c0ef5d3ad1e8c4d2df3b66c0251f389cf9a9
alvinnyen <alvinyen219@gmail.com>
2021-02-14 04:51:05 +0800

    add some error handlings to Table Component

--- src/pages/PageShoppingCheckoutResult/components/Table/index.js
+++ src/pages/PageShoppingCheckoutResult/components/Table/index.js
@@ -3,10 +3,13 @@ import "./Table.css";
 export default (props = {}) => {
   const { columnWidths = [], headerColumns = [], dataRows = [] } = props;
 
+  // it's meaningless to render a table if no data rows
+  if (!dataRows.length) return null;
+
   const renderColumns = (columns = [], columnWidths = []) => {
     return columns.map((column = "", index) => {
       const style = {
-        width: columnWidths[index],
+        width: columnWidths[index] || `${100 / columns.length}%`,
       };
       return (
         <div key={index} className="cell" style={style}>
