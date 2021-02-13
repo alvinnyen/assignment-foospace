0a9fc765b104054f76c3f4d0fc2a7543974d6963
alvinnyen <alvinyen219@gmail.com>
2021-02-14 00:29:54 +0800

    add getCampaigns and getCampaignPriorities in the utils of ButtonCheckout

--- src/pages/PageShopping/components/ButtonCheckout/utils/index.js
+++ src/pages/PageShopping/components/ButtonCheckout/utils/index.js
@@ -1,4 +1,4 @@
-function loadProduct(productListArray = [], productsData = {}) {
+const loadProduct = (productListArray = [], productsData = {}) => {
   return productListArray.map((productId = "", index) => {
     const { productName = "", productPrice = 0 } = productsData[productId];
     return {
@@ -8,9 +8,36 @@ function loadProduct(productListArray = [], productsData = {}) {
       positionInProducts: index,
     };
   });
-}
+};
+
+const getCampaigns = () => {
+  // maybe need to get campaigns from somewhere in the future
+  const campaigns = {
+    // TODO: check 名字在外還在內欄位好，欄位命名是否ＯＫ
+    buyOneGetOneInHalfPrice: {
+      desc: "同商品第 2 件 5 折",
+      // processFunc: buyOneGetOneInHalfPrice,
+    },
+    reduceFiveDollarsForEveryProductWithThreeAnyProducts: {
+      desc: "任意商品(可相同也可不同)滿 3 件以上每件皆折 5 元",
+      // processFunc: reduceFiveDollarsForEveryProductWithThreeAnyProducts,
+    },
+  };
+  return campaigns;
+};
+
+const getCampaignPriorities = () => {
+  // maybe need to get campaign priorities from somewhere in the future
+  const campaignsInPriorities = [
+    "buyOneGetOneInHalfPrice",
+    "reduceFiveDollarsForEveryProductWithThreeAnyProducts",
+  ];
+  return campaignsInPriorities;
+};
 
 export default (productListArray = [], productsData = {}) => {
   let products = loadProduct(productListArray, productsData);
-  console.log(products);
+  const campaigns = getCampaigns();
+  const campaignPriorities = getCampaignPriorities();
+  console.log(campaignPriorities);
 };
