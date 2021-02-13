0b24bbb0ef98e85eb603950619af41a3e198f6f8
alvinnyen <alvinyen219@gmail.com>
2021-02-14 00:21:18 +0800

    add loadProduct in the utils of ButtonCheckout

--- src/pages/PageShopping/components/ButtonCheckout/utils/index.js
+++ src/pages/PageShopping/components/ButtonCheckout/utils/index.js
@@ -1,5 +1,16 @@
-export default (productListArray = [], productsData = []) => {
-  console.log(productListArray);
-  console.log(productsData);
-  console.log("");
+function loadProduct(productListArray = [], productsData = {}) {
+  return productListArray.map((productId = "", index) => {
+    const { productName = "", productPrice = 0 } = productsData[productId];
+    return {
+      productId,
+      productName,
+      productPrice,
+      positionInProducts: index,
+    };
+  });
+}
+
+export default (productListArray = [], productsData = {}) => {
+  let products = loadProduct(productListArray, productsData);
+  console.log(products);
 };
