e694e65e0b90ecf65aa9f9c164d373f45ce56c89
alvinnyen <alvinyen219@gmail.com>
2021-02-14 02:04:09 +0800

    use useContext refactoring ButtonCheckout

--- src/pages/PageShopping/components/ButtonCheckout/index.js
+++ src/pages/PageShopping/components/ButtonCheckout/index.js
@@ -1,34 +1,37 @@
-import { Consumer as ConsumerContextProductList } from "../../../../contexts/ContextProductList";
-import utilCheckout from "./utils";
+import { useContext } from "react";
+
+import contextProductList from "../../../../contexts/ContextProductList";
+import utilGetProcessedProducts from "./utils";
 
 export default (props = {}) => {
+  const { productList = "", setProductList = () => {} } = useContext(
+    contextProductList
+  );
+
+  const buttonDisabled = !productList;
+
+  const handleCheckoutButtOnClick = () => {
+    const productListArray = productList.split(",");
+    const { productsData = {} } = props;
+    // console.log(productList);
+    // console.log(productListArray);
+    // console.log(productsData);
+    // console.log(" ");
+    const processedProducts = utilGetProcessedProducts(
+      productListArray,
+      productsData
+    );
+    console.log(processedProducts);
+    console.log(" ");
+  };
+
   return (
-    <ConsumerContextProductList>
-      {(value = {}) => {
-        const { productList = "", setProductList = () => {} } = value;
-        const buttonDisabled = !productList;
-
-        const handleCheckoutButtOnClick = () => {
-          const productListArray = productList.split(",");
-          const { productsData = {} } = props;
-          // console.log(productList);
-          // console.log(productListArray);
-          // console.log(productsData);
-          // console.log(" ");
-          const total = utilCheckout(productListArray, productsData);
-          console.log(total);
-        };
-
-        return (
-          <button
-            className="checkout-button"
-            onClick={handleCheckoutButtOnClick}
-            disabled={buttonDisabled}
-          >
-            Checkout
-          </button>
-        );
-      }}
-    </ConsumerContextProductList>
+    <button
+      className="checkout-button"
+      onClick={handleCheckoutButtOnClick}
+      disabled={buttonDisabled}
+    >
+      Checkout
+    </button>
   );
 };
