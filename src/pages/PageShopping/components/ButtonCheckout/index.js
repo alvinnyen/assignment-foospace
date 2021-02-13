91178649cde20b756c0160c5543674cfe9063770
alvinnyen <alvinyen219@gmail.com>
2021-02-13 23:28:27 +0800

    connect utilCheckout and ContextProductList to ButtonCheckout to implement checkout logic

--- src/pages/PageShopping/components/ButtonCheckout/index.js
+++ src/pages/PageShopping/components/ButtonCheckout/index.js
@@ -1,3 +1,25 @@
+import { Consumer as ConsumerContextProductList } from "../../../../contexts/ContextProductList";
+import utilCheckout from "./utils";
+
 export default () => {
-  return <button className="checkout-button">Checkout</button>;
+  return (
+    <ConsumerContextProductList>
+      {(value = {}) => {
+        const { productList = "", setProductList = () => {} } = value;
+
+        const handleCheckoutButtOnClick = () => {
+          utilCheckout(productList);
+        };
+
+        return (
+          <button
+            className="checkout-button"
+            onClick={handleCheckoutButtOnClick}
+          >
+            Checkout
+          </button>
+        );
+      }}
+    </ConsumerContextProductList>
+  );
 };
