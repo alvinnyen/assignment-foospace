033c5738de2eddf5c3374d0e7553e699fcb5e68b
alvinnyen <alvinyen219@gmail.com>
2021-02-14 05:14:42 +0800

    refactor Product with useContext

--- src/pages/PageShopping/components/Product/index.js
+++ src/pages/PageShopping/components/Product/index.js
@@ -1,33 +1,32 @@
-import "./Product.css";
+import { useContext } from "react";
 
-import { Consumer as ConsumerContextProductList } from "../../../../contexts/ContextProductList";
+import contextProductList from "../../../../contexts/ContextProductList";
+
+import "./product.css";
 
 export default (props = {}) => {
   const {
     product: { productId = "", productName = "", productPrice = 0 } = {},
   } = props;
   if (!productId || !productName || !productPrice) return null;
-  return (
-    <ConsumerContextProductList>
-      {(value = {}) => {
-        const handleProductOnClick = () => {
-          const {
-            productList: prevProductList = "",
-            setProductList = () => {},
-          } = value;
-          const productList = prevProductList
-            ? prevProductList + "," + productId
-            : productId;
 
-          setProductList(productList);
-        };
-        return (
-          <div className="wrapper" onClick={handleProductOnClick}>
-            <div>product name: {productName}</div>
-            <div>product price: {productPrice}</div>
-          </div>
-        );
-      }}
-    </ConsumerContextProductList>
+  const {
+    productList: prevProductList = "",
+    setProductList = () => {},
+  } = useContext(contextProductList);
+
+  const handleProductOnClick = () => {
+    const productList = prevProductList
+      ? prevProductList + "," + productId
+      : productId;
+
+    setProductList(productList);
+  };
+
+  return (
+    <div className="wrapper" onClick={handleProductOnClick}>
+      <div>product name: {productName}</div>
+      <div>product price: {productPrice}</div>
+    </div>
   );
 };
