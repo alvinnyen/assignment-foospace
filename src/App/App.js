fe805582b01d85c7eff449c96bd52f3149825e13
alvinnyen <alvinyen219@gmail.com>
2021-02-14 19:15:10 +0800

    apply ContextProducts and ContextProductsData in App and move the api call and useEffect from PageShopping to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import PageShopping from "../pages/PageShopping";
 import PageShoppingCheckoutResult from "../pages/PageShoppingCheckoutResult";
 import Composer from "./hocs/Composer";
 
+import { apiGetProducts } from "../apis/products/apiProducts";
+
 import { Provider as ContextPageProvider } from "../contexts/ContextPage";
+import { Provider as ContextProductsProvider } from "../contexts/ContextProducts";
+import { Provider as ContextProductsDataProvider } from "../contexts/ContextProductsData";
 import { Provider as ContextProcessedProductsProvider } from "../contexts/ContextProcessedProducts";
 
 const pages = {
@@ -15,6 +19,35 @@ const pages = {
 function App() {
   const [page, setPage] = useState(pages.PAGE_SHOPPING);
   const [processedProducts, setProcessedProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [productsData, setProductsData] = useState({});
+
+  useEffect(async () => {
+    const { products: productsFromApi = {} } = await apiGetProducts();
+    const products = [];
+    const productsData = {};
+
+    for (let productId in productsFromApi) {
+      const {
+        name: productName = "",
+        price: productPrice = 0,
+      } = productsFromApi[productId];
+
+      // ensure the fields of a product from api/database are fine,
+      // then push it to the products array,
+      // so that can be rendered properly later
+      productName &&
+        productPrice > 0 &&
+        products.push({
+          productId,
+          productName,
+          productPrice,
+        }) &&
+        (productsData[productId] = { productName, productPrice });
+    }
+    setProducts(products);
+    setProductsData(productsData);
+  }, []);
 
   const renderPage = (page = "") => {
     const { PAGE_SHOPPING = "", PAGE_SHOPPING_CHECKOUT_RESULT = "" } = pages;
@@ -42,6 +75,20 @@ function App() {
       contextValue: {
         processedProducts,
         setProcessedProducts,
+      },
+    },
+    {
+      contextProvider: ContextProductsProvider,
+      contextValue: {
+        products,
+        setProducts,
+      },
+    },
+    {
+      contextProvider: ContextProductsDataProvider,
+      contextValue: {
+        productsData,
+        setProductsData,
       },
     },
   ];
