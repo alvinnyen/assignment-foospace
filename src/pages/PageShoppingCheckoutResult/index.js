25329d91ee601fb4fa25ff571384210daaf6c4fa
alvinnyen <alvinyen219@gmail.com>
2021-02-15 20:51:31 +0800

    add default values to variables in PageShoppingCheckoutResult for preventing crash

--- src/pages/PageShoppingCheckoutResult/index.js
+++ src/pages/PageShoppingCheckoutResult/index.js
@@ -10,55 +10,66 @@ import "./PageShoppingCheckoutResult.css";
 export default () => {
   const { processedProducts = [] } = useContext(contextProcessedProducts);
 
-  const columnWidths = ["10%", "10%", "10%", "10%", "60%"];
-  const headerColumns = ["#", "ID", "原價", "活動價", ""];
-
-  const getCampaignWording = (data = {}) => {
-    const { campaign = "", related = [] } = data;
-    if (related.length) {
-      return `(與第 ${related.join("、")} 項組合成${campaign})`;
+  const renderTableAndTotalWithTheWording = (processedProducts = []) => {
+    if (!processedProducts.length) {
+      return null;
     }
-    return (campaign && `(${campaign})`) || "";
-  };
-  const dataRows = processedProducts.map((processedProduct = {}, index) => {
-    const {
-      campaign = "",
-      productId = "",
-      productPrice = 0,
-      related = [],
-      discount = 0,
-    } = processedProduct;
 
-    const campaignWording = getCampaignWording({
-      campaign,
-      related,
+    const columnWidths = ["10%", "10%", "10%", "10%", "60%"];
+    const headerColumns = ["#", "ID", "原價", "活動價", ""];
+
+    const getCampaignWording = (data = {}) => {
+      const { campaign = "", related = [] } = data;
+      if (related.length) {
+        return `(與第 ${related.join("、")} 項組合成${campaign})`;
+      }
+      return (campaign && `(${campaign})`) || "";
+    };
+    const dataRows = processedProducts.map((processedProduct = {}, index) => {
+      const {
+        campaign = "",
+        productId = "",
+        productPrice = 0,
+        related = [],
+        discount = 0,
+      } = processedProduct;
+
+      const campaignWording = getCampaignWording({
+        campaign,
+        related,
+      });
+
+      // "#", "ID", "原價", "活動價", ""
+      return [
+        index + 1 + "",
+        productId,
+        productPrice,
+        productPrice - discount,
+        campaignWording,
+      ];
     });
 
-    // "#", "ID", "原價", "活動價", ""
-    return [
-      index + 1 + "",
-      productId,
-      productPrice,
-      productPrice - discount,
-      campaignWording,
-    ];
-  });
-
-  const total = processedProducts.reduce((accuTotal = 0, product = {}) => {
-    const { productPrice = 0, discount = 0 } = product;
-    return (accuTotal += productPrice - discount);
-  }, 0);
-  const totalWithTheWording = `價格為$${total}`;
+    const total = processedProducts.reduce((accuTotal = 0, product = {}) => {
+      const { productPrice = 0, discount = 0 } = product;
+      return (accuTotal += productPrice - discount);
+    }, 0);
+    const totalWithTheWording = total && `價格為$${total}`;
+    return (
+      <>
+        <Table
+          columnWidths={columnWidths}
+          headerColumns={headerColumns}
+          dataRows={dataRows}
+        />
+        <div className="total-with-the-wording">{totalWithTheWording}</div>
+      </>
+    );
+  };
 
   return (
     <div>
       <h1>PageShoppingCheckoutResult</h1>
-      <Table
-        columnWidths={columnWidths}
-        headerColumns={headerColumns}
-        dataRows={dataRows}
-      />
-      <div className="total-with-the-wording">{totalWithTheWording}</div>
+      {renderTableAndTotalWithTheWording(processedProducts)}
       <ButtonBack />
     </div>
   );
